65add5c391c232b73976c888eadc75b7a044d2b4
MiaFate <miapurplefroot@gmail.com>
2021-08-25 20:31:26 -0300

    cards are links now instead of just the images

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -5,23 +5,25 @@ const Profile = (props) => {
   const { profile } = props;
 
   return (
+    <Link className="link-text" to={{ pathname: "/FullProfile", profile: profile }}>
     <div className="card">
-      <div className="card-image">
-        <Link to={{ pathname: "/FullProfile", profile: profile }}>
+      
+        <div className="card-image">
           <img
             src={profile.picture.thumbnail}
             alt={`${profile.name.first} ${profile.name.last}`}
             height="48px"
             width="48px"
           />
-        </Link>
-      </div>
-      <div className="card-data">
-      <p className="fullname">{`${profile.name.title} ${profile.name.first} ${profile.name.last}`}</p>
-      <p>{profile.location.city}</p>
-      <p>{profile.location.country}</p>
-      </div>
+        </div>
+        <div className="card-data">
+          <p className="fullname">{`${profile.name.title} ${profile.name.first} ${profile.name.last}`}</p>
+          <p>{profile.location.city}</p>
+          <p>{profile.location.country}</p>
+        </div>
+      
     </div>
+    </Link>
   );
 };
 export default Profile;
